38ec45eacb2081e6ea951f16963ab1b8cd2cdeb6
mgv <mglasner10@gmail.com>
2023-08-27 22:07:56 -0400

    bi-directional foundation analysis

--- fastruct/commands/analysis/app.py
+++ fastruct/commands/analysis/app.py
@@ -1,4 +1,6 @@
-"""Comandos para el módulo analisis."""
+"""Analysis Commands"""
+from typing import Literal, Optional
+
 import typer
 from config_db import session_scope
 from models.foundation import Foundation
@@ -6,20 +8,27 @@ from rich.console import Console
 from rich.table import Table
 from rich.text import Text
 
-from .functions import get_stress, lifting_percentaje
+from .functions import get_bi_directional_percentaje_and_stress, get_stress, lifting_percentaje
 
 app = typer.Typer()
 console = Console()
 
 
-@app.command(name="tl")
-def tensiones_y_levantamientos(fundacion_id: int, verbose: bool = False):
-    """Análisis de tensiones máximas y levantamientos."""
+@app.command(name="run")
+def analyze_stresses_and_lifts(
+    foundation_id: int, method: Optional[str] = "bi-direction", no_loads: bool = False
+) -> None:
+    """Analyze maximum stresses and lifts.\n
+
+    This function takes the ID of a foundation, fetches the foundation data and then\n
+    computes the maximum stresses and lifts occurring in the foundation.\n
+
+    Args:\n
+        foundation_id (int): The ID of the foundation to analyze.\n
+    """
     with session_scope() as session:
-        foundation = session.query(Foundation).filter_by(id=fundacion_id).first()
-        table = Table("#", "NAME", "P", "Vx", "Vy", "Mx", "My", "σx max", r"%x", "σy max", r"%y")
-        table.title = Text(str(foundation), style="black on white bold")
-        table.show_lines = True
+        foundation = session.query(Foundation).filter_by(id=foundation_id).first()
+        table = config_table(str(foundation), method, no_loads)  # type: ignore
 
         liftings = lifting_percentaje(foundation)
         stresses = get_stress(foundation)
@@ -57,6 +66,10 @@ def tensiones_y_levantamientos(fundacion_id: int, verbose: bool = False):
                 else f"{percentaje_y:.2f}"
             )
 
+            bi_directional_max_stress, bi_directional_percentaje = get_bi_directional_percentaje_and_stress(
+                foundation, load
+            )
+
             row = [
                 f"{i:02}",
                 f"{load.user_load.name}",
@@ -65,12 +78,36 @@ def tensiones_y_levantamientos(fundacion_id: int, verbose: bool = False):
                 f"{load.vy:.1f}",
                 f"{load.mx:.1f}",
                 f"{load.my:.1f}",
-                sigma_x,
-                percentaje_x,
-                sigma_y,
-                percentaje_y,
+                # sigma_x,
+                # percentaje_x,
+                # sigma_y,
+                # percentaje_y,
+                f"{bi_directional_max_stress:.2f}" if bi_directional_max_stress is not None else "∞",
+                f"{bi_directional_percentaje:.2f}",
             ]
 
             table.add_row(*row)
 
     console.print(table)
+
+
+def config_table(title: str, method: Literal["bi-directional", "one-direction", "compare"], no_loads: bool) -> Table:
+    """Table configuration."""
+    columns = ["#", "NAME"]
+    if not no_loads:
+        columns.extend(["P", "Vx", "Vy", "Mx", "My"])
+
+    if method == "bi-directional":
+        columns.extend(["σ (ton/m²)", "%"])
+
+    elif method == "one-direction":
+        columns.extend(["σx max", r"%x", "σy max", r"%y"])
+
+    elif method == "compare":
+        columns.extend(["σx max", r"%x", "σy max", r"%y", "σ (ton/m²)", "%"])
+
+    table = Table(*columns)
+    table.title = Text(title, style="black on white bold")
+    table.show_lines = True
+
+    return table
